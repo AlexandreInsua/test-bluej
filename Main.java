6b96ddc65bcdf0d569e0100544d3aa3a874fe213
Alexandre Insua <alexandreinsua@gmail.com>
2023-01-18 19:59:59 +0100

    Intercambio inicial de proyecto

---
+++ Main.java
@@ -0,0 +1,23 @@
+
+/**
+ * Description
+ * 
+ * @author 
+ * @version 
+ */
+public class Main
+{
+ 
+    public static void main (String[] args){
+        Main m = new Main();
+    }
+    /**
+     * Constructor for objects of class Main
+     */
+    public Main()
+    {
+        
+    }
+
+ 
+}
